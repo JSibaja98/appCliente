a94fcb1bdf9ef453f032821d09ead667bc50de72
eduardo-salas <eduardo.salas@sevensoftit.com>
2020-08-24 22:42:43 -0600

    Update products_cards.dart

--- lib/src/utils/products_cards.dart
+++ lib/src/utils/products_cards.dart
@@ -33,51 +33,51 @@ class ProductsCards extends StatelessWidget {
   }
 
   Widget _cardList(BuildContext context, List<ProductsModel> product) {
-    return GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-        ),
-        itemCount: product.length,
-        itemBuilder: (context, index) {
-          final card = Container(
-            child: Column(
-              children: <Widget>[
-                FadeInImage(
-                  placeholder: AssetImage('assets/img/loading.gif'),
-                  image: NetworkImage(
-                      'https://www.nationalgeographic.com/content/dam/travel/rights-exempt/2019-travel-photo-contest/epic-landscapes/2019-travel-photo-contest-epic-landscapes031.jpg'),
-                  fadeInDuration: Duration(milliseconds: 300),
-                  height: 200.0,
-                  fit: BoxFit.cover,
-                ),
-                Container(
-                  padding: EdgeInsets.all(10.0),
-                  child: Text('this is an image'),
-                ),
-              ],
-            ),
-          );
+    // return GridView.builder(
+    //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+    //       crossAxisCount: 2,
+    //     ),
+    //     itemCount: product.length,
+    //     itemBuilder: (context, index) {
+    final card = Container(
+      child: Column(
+        children: <Widget>[
+          FadeInImage(
+            placeholder: AssetImage('assets/img/loading.gif'),
+            image: NetworkImage(
+                'https://www.nationalgeographic.com/content/dam/travel/rights-exempt/2019-travel-photo-contest/epic-landscapes/2019-travel-photo-contest-epic-landscapes031.jpg'),
+            fadeInDuration: Duration(milliseconds: 300),
+            height: 200.0,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('this is an image'),
+          ),
+        ],
+      ),
+    );
 
-          return Container(
-            width: 180.0,
-            margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 15.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              color: Colors.white,
-              boxShadow: <BoxShadow>[
-                BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 10.0,
-                    spreadRadius: 2.0,
-                    offset: Offset(3.0, 4.0))
-              ],
-            ),
-            child: ClipRRect(
-              child: card,
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-          );
-        });
+    return Container(
+      width: 180.0,
+      margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+              color: Colors.black26,
+              blurRadius: 10.0,
+              spreadRadius: 2.0,
+              offset: Offset(3.0, 4.0))
+        ],
+      ),
+      child: ClipRRect(
+        child: card,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+    // });
   }
 
   Widget _card(BuildContext context, ProductsModel product) {
