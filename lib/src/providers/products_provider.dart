e84e29e020b868fe5a573438d6612b064ebee26b
eduardo-salas <eduardo.salas@sevensoftit.com>
2020-08-24 21:10:46 -0600

    Update products_provider.dart

--- lib/src/providers/products_provider.dart
+++ lib/src/providers/products_provider.dart
@@ -5,9 +5,11 @@ import 'package:proyectorestclient/src/models/product_model.dart';
 
 class ProductsProvider {
 
-    String _apiKey = 'aaeb1f745a530717c0dafd9b9b699bf6';
-  String _url = 'api.themoviedb.org';
-  String _language = 'en-Us';
+  //   String _apiKey = 'aaeb1f745a530717c0dafd9b9b699bf6';
+  // String _url = 'api.themoviedb.org';
+    String _url = 'localhost:5000';
+
+  // String _language = 'en-Us';
 
 /**************************************************************************************
  * Author: Esalas
@@ -17,24 +19,23 @@ class ProductsProvider {
  * Powerd by: getCasting
  * ************************************************************************************/
   Future<String> getProducts() async {
-    // final url = '$_url/productos.json';
-    // final url = http.get(new Uri.http("$_url", "/productos"));
-    final url = Uri.https(_url, '3/movie/popular', {
-      'api_key': _apiKey,
-      'language': _language,
-    });
-    // final url = http.get('https://jsonplaceholder.typicode.com/albums/1');
+
+    final url = new Uri.http(_url, "/productos");
+    // final url = Uri.https(_url, '3/movie/popular', {
+    //   'api_key': _apiKey,
+    //   'language': _language,
+    // });
     final resp = await http.get(url);
     final Map<String, dynamic> decodedData = json.decode(resp.body);
-    // final List<ProductsModel> products = new List();
+    final List<ProductsModel> products = new List();
 
     if (decodedData == null) {
       return '';
     } else {
       decodedData.forEach((id, item) {
         print(item);
-        // final prodTemp = ProductsModel.fromJson(prod);
-        // products.add(prodTemp);
+        final prodTemp = ProductsModel.fromJson(item);
+        products.add(prodTemp);
       });
     }
 
